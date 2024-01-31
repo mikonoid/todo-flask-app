d6bfa477874bfafb985765f913968c5d896f35ad
Mike Ivanov <mikonoid@gmail.com>
2024-01-31 16:26:31 +0200

    fix CI

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-MONGO_URI = 'mongodb://root:examplepass@todo-todo-chart-mongodb.todo.svc.cluster.local:27017'
+MONGO_URI = 'mongodb://root:examplepass@mongodb.todo.svc.cluster.local:27017'
 MONGO_DB = 'todo'
